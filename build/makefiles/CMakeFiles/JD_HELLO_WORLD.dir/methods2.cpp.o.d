e907030249141b06adf1743af8bc3f3bbe3caf77
jdyun <wjdeh313@naver.com>
2022-07-14 19:28:58 +0900

    fsefse

--- build/makefiles/CMakeFiles/JD_HELLO_WORLD.dir/methods2.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/JD_HELLO_WORLD.dir/methods2.cpp.o: \
- /home/jdyun/Programming/cmake-test/methods2.cpp \
- /usr/include/stdc-predef.h
--- main.cpp
+++ main.cpp
@@ -2,11 +2,9 @@
 
 extern const char* msg();
 extern const char* name();
-extern int number();
 
 int main() { 
     std::cout << msg() << "\n";
     std::cout << "my name is " << name() << "\n";
-    std::cout << number() << "\n";
     return 0;
 }
