b2ef09875802c2b2b10a41bc1ea839a583e76eea
jdyun <wjdeh313@naver.com>
2022-07-14 16:09:12 +0900

    1

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 
 extern const char* msg();
 extern const char* name();
+extern int number();
 
 int main() { 
     std::cout << msg() << "\n";
     std::cout << "my name is " << name() << "\n";
+    std::cout << number() << "\n";
     return 0;
 }
